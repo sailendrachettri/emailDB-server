87e21aa9de91319d92897e987fa06992a86a459a
sailendrachettri <sailendra9083@gmail.com>
2024-06-01 23:31:21 +0530

    user login and signup api written

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config({path: ['.env.dev', '.env']});
 require('./database');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 8000
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,56 @@
 const express = require('express');
+const User = require('../models/user.models');
+const bcryptjs = require('bcryptjs');
 
 const router = express.Router();
 
 // ROUTE 1: save user info to database
-router.post('/register', (req, res)=>{
-    res.send("hello");
+router.post('/register', async(req, res)=>{
+
+    const {username, password} = req.body
+
+    if(!(username && password)){
+        return res.status(400).json({success: false, message: "Username and Password are required"});
+    }
+    
+    const alreadyExist = await User.findOne({username});
+    console.log(alreadyExist);
+    
+    if(alreadyExist){
+       return res.status(400).json({success: false, message: "Account with this username already exist"});;
+    }
+
+    const hashPassword = bcryptjs.hashSync(password, 10);
+
+    const user = await User.create({username, password: hashPassword});
+
+    res.status(200).json({success: true, message: "User created successfully!", user});
+
 })
 
 // ROUTE 2: login the user
-router.post('/login', (req, res)=>{
-    res.send("hello");
+router.post('/login', async(req, res)=>{
+    const {username, password} = req.body;
+
+    if(!(username && password)){
+        return res.status(400).json({success: false, message: "Username and Password are required"});
+    }
+
+    const findUser = await User.findOne({username});
+
+    if(!findUser){
+        return res.status(404).json({success: false, message: "User doesn't exist"});
+    }
+
+    console.log(findUser);
+
+    const comparePassword = bcryptjs.compareSync(password, findUser.password);
+
+    if(!comparePassword){
+        return  res.status(400).json({success: false, message: "Invalid credentials"});
+    }
+
+    res.status(200).json({success: true, message: "Logged in successful", findUser});
 })
 
 module.exports = router
